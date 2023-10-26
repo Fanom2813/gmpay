954ea24e5dd71a67b61ec7a77e220810f923f840
Fanom2813 <40917429+Fanom2813@users.noreply.github.com>
2023-10-26 09:03:52 +0300

    upd url

--- lib/src/common/constants.dart
+++ lib/src/common/constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
-  // static const baseUrl = "https://api.gmpayapp.com/api/v2";
-  static const baseUrl = "http://localhost:8000/api/v2";
+  static const baseUrl = "https://api.gmpayapp.com/api/v2";
+  // static const baseUrl = "http://localhost:8000/api/v2";
 }
